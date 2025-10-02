4e5e00cb3f7a733a330adf05ec2f74c9d5536808
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2025-10-02 14:06:35 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,31 @@
+function listo(nombre, email, mensaje){
+    if (nombre.trim() === ""){
+        alert("Se espera que ingrese su nombre.")
+    } else if (email.trim() === ""){
+        alert("Se espera que ingrese su e-mail.")
+    } else if (mensaje.trim() === ""){
+        alert("No se puede enviar un mensaje en blanco..")
+    } else {
+        alert("¡Mensaje enviado, " + nombre + "! Gracias por contactarte.")
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let backToTopBtn = document.getElementById("back-to-top-btn");
+
+    // Mostrar / ocultar el botón al scrollear
+    //window.addEventListener("scroll", () => {
+      //  if (window.scrollY > 100) {
+       //     backToTopBtn.classList.add("show");
+        //} else {
+          //  backToTopBtn.classList.remove("show");
+        //}
+    //});
+
+    // Scroll suave al inicio
+    backToTopBtn.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+
+});
