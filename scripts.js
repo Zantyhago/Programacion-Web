bb327611dadb6e38743e4e1c60c3d52c18246fee
Santiago Sanchez <167319346+Zantyhago@users.noreply.github.com>
2025-10-13 09:51:25 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 function listo(nombre, email, mensaje){
-    if (nombre.trim() === ""){
+    if ((nombre.trim() === "") && (email.trim() === "") && (mensaje.trim() === "")){
+        alert("Los campos están vacíos.")
+    }else if (nombre.trim() === ""){
         alert("Se espera que ingrese su nombre.")
     } else if (email.trim() === ""){
         alert("Se espera que ingrese su e-mail.")
@@ -11,23 +13,17 @@ function listo(nombre, email, mensaje){
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    let backToTopBtn = document.getElementById("back-to-top-btn");
-
-    // Mostrar / ocultar el botón al scrollear
-    //window.addEventListener("scroll", () => {
-      //  if (window.scrollY > 100) {
-       //     backToTopBtn.classList.add("show");
-        //} else {
-          //  backToTopBtn.classList.remove("show");
-        //}
-    //});
-
-    // Scroll suave al inicio
+    let backToTopBtn = document.getElementById("subir-btn");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            backToTopBtn.classList.add("show");
+        } else {
+            backToTopBtn.classList.remove("show");
+        }
+    });
     backToTopBtn.addEventListener("click", () => {
         window.scrollTo({ top: 0, behavior: 'smooth' });
     });
-
-
 });
 
 const footer = document.querySelector("footer");
@@ -35,3 +31,14 @@ const fecha = new Date();
 const pF = document.createElement("p");
 pF.textContent = "Fecha actual: " + fecha.toLocaleDateString("es-ES");
 footer.appendChild(pF);
+
+function CambioTema(background, bg_url, bg_divs, fcolor){
+    document.body.style.backgroundColor = background;
+    document.body.style.backgroundImage = `url(${bg_url})`;
+    document.getElementById('sobre-mi').style.backgroundColor = bg_divs;
+    document.getElementById('proyectos').style.backgroundColor = bg_divs;
+    document.getElementById('contacto').style.backgroundColor = bg_divs;
+    document.getElementById('referencias').style.backgroundColor = bg_divs;
+    document.querySelector('aside').style.color = fcolor;
+    document.querySelector('footer').style.color = fcolor;
+}
